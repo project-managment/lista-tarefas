95081e41d62871801acbf246148ddd5c1819cb40
Tiago Lisboa <tiago.caio.ol@gmail.com>
2018-11-08 14:01:47 -0300

    Alterações Relacionadas a Tarefa #9 e #12

--- src/components/pages/Listar.js
+++ src/components/pages/Listar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Table, Grid, Row, Col, Checkbox} from 'react-bootstrap';
+import {Table, Grid, Row, Col, Checkbox, Button} from 'react-bootstrap';
 import {getToken} from '../tils/Auth';
 import API from '../tils/Api';
 
@@ -9,6 +9,24 @@ export default class Listar extends Component{
     this.state = {
       tarefas: []
     }
+
+    this.apagarTarefa = evento => {
+      evento.persist();
+      var array = [...this.state.tarefas];
+      array.splice(evento.target.getAttribute('pos') ,1);
+      this.setState({
+        tarefas: array
+      })
+    };
+
+    this.tarefaFeita = evento => {
+      evento.persist();
+      var array = [...this.state.tarefas];
+      array.splice(evento.target.getAttribute('pos') ,1);
+      this.setState({
+        tarefas: array
+      })
+    };
   }
 
   componentWillMount(){
@@ -25,34 +43,36 @@ export default class Listar extends Component{
   render(){
     return(
      <Grid>
-     <Row>
-     <Col md={2}/>
-     <Col md={8}>
-      <Table striped bordered condensed hover>
-     <thead>
-     <tr>
-     <th></th>
-     <th>ID</th>
-     <th>TÍTULO</th>
-     <th>DESCRIÇÃO</th>
-     </tr>
-     </thead>
-     <tbody>
+        <Row>
+          <Col md={2}/>
+          <Col md={8}>
+            <Table striped bordered condensed hover>
+            <thead>
+             <tr>
+                <th>ID</th>
+                <th>TÍTULO</th>
+                <th>DESCRIÇÃO</th>
+                <th></th>
+                <th></th>
+             </tr>
+             </thead>
+             <tbody>
       {
         this.state.tarefas.map((row, index) => (
           <tr>
-          <td>  <Checkbox></Checkbox> </td>
-          <td>{row.id}</td>
-          <td>{row.title}</td>
-          <td>{row.description}</td>
+            <td>{row.id}</td>
+            <td>{row.title}</td>
+            <td>{row.description}</td>
+            <td><Button id={row.id} pos={index} bsStyle="success" onClick={this.tarefaFeita}>FEITA</Button></td>
+            <td><Button id={row.id} pos={index} bsStyle="danger" onClick={this.apagarTarefa}>APAGAR</Button></td>
           </tr>
         ))
       }
-      </tbody>
-      </Table>
-      </Col>
-      <Col md={2}/>
-      </Row>
+            </tbody>
+            </Table>
+          </Col>
+          <Col md={2}/>
+        </Row>
       </Grid>
     );
   }
